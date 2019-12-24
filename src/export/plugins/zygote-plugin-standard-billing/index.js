770314ccbb0fd4a4937b87b3ca37286a14a1d01b
Jacob <jgsimmerman@gmail.com>
2019-12-24 14:49:03 -0500

    first commit

---
+++ src/export/plugins/zygote-plugin-standard-billing/index.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react'
+
+import NameInput from '../../components/inputs/name'
+import CreditCard from '../../components/inputs/credit-card'
+import Expiration from '../../components/inputs/expiration'
+import Cvc from '../../components/inputs/cvc'
+
+export class Payment extends React.Component {
+	render() {
+		return (
+			<Fragment>
+				
+			</Fragment>
+		)
+	}
+}
+
